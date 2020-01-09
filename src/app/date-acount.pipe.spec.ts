91d07b1c1153d3e55a30bbc54ed1a7aab92ef0ec
benson7-coder <bensonowino7@gmail.com>
2020-01-09 10:29:34 +0300

    added json broewselist html css ts and js files

---
+++ src/app/date-acount.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateAcountPipe } from './date-acount.pipe';
+
+describe('DateAcountPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateAcountPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
